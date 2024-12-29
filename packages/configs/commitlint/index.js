60877190cf5383c61f23bc11265de2f0b2dbb24b
Mikhail Cheremuhin-Rerberg <103729911+mrerberg@users.noreply.github.com>
2024-12-29 17:07:46 +0300

    fix(*): Add "-" symbol as valid symbol in commitlint (#15)

--- packages/configs/commitlint/index.js
+++ packages/configs/commitlint/index.js
@@ -1,14 +1,14 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    "scope-empty": [2, "never"],
-    "message-regex": [2, "always"],
+    'scope-empty': [2, 'never'],
+    'message-regex': [2, 'always'],
   },
   plugins: [
     {
       rules: {
-        "message-regex": ({ subject }) => {
-          const onlyEnglishCharactersRegex = /^[a-zA-Z0-9\s.,!?'"]+$/;
+        'message-regex': ({ subject }) => {
+          const onlyEnglishCharactersRegex = /^[a-zA-Z0-9\s.,-!?'"]+$/;
           return [
             onlyEnglishCharactersRegex.test(subject),
             `The commit message must be in English. Your message: "${subject}".`,
